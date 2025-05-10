c5a8b2c9c4c563e4a9e4dc9e884ade5e3ac3ad85
KamalSharma13 <kamal.webdev13@gmail.com>
2025-05-10 17:42:33 +0530

    update staging credentials

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import Config from "config";
 import Routes from "./routes";
 import Server from "./common/server";
 // const dbUrl = `mongodb://${Config.get('databaseHost')}:${Config.get("databasePort")}/${Config.get("databaseName")}`;
-const dbUrl =`mongodb+srv://kamalwebdev13:${Config.get("databasePass")}@cluster0.5il0wke.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const dbUrl =`mongodb+srv://eleglam:${Config.get("databasePass")}@cluster0.n4ngtoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 const server = new Server()
   .router(Routes)
   .configureSwagger(Config.get("swaggerDefinition"))
